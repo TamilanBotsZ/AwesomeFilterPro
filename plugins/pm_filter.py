6b9b45067ddc2d46dce81b7a10fb2208d54dac28
technical_aks <105633032+technicalaks@users.noreply.github.com>
2023-01-31 22:09:14 +0530

    Cng load effect

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -450,13 +450,13 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text="▣▢▢"
+            text="▮▯▯"
         )
         await query.message.edit_text(
-            text="▣▣▢"
+            text="▮▮▯"
         )
         await query.message.edit_text(
-            text="▣▣▣"
+            text="▮▮▮"
         )
         await query.message.edit_text(
             text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
@@ -493,13 +493,13 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text="▣▢▢"
-        )
-        await query.message.edit_te
-            text="▣▣▢"
+            text="▮▯▯"
         )
         await query.message.edit_text(
-            text="▣▣▣"
+            text="▮▮▯"
+        )
+        await query.message.edit_text(
+            text="▮▮▮"
         )
         await query.message.edit_text(
         reply_markup = InlineKeyboardMarkup(buttons)
