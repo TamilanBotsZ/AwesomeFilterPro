c6542ab13e210988a2047bbe3900446a63b560c7
technical_aks <105633032+technicalaks@users.noreply.github.com>
2023-01-31 21:04:46 +0530

    Fixed by @Imdb_updates

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -501,6 +501,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.message.edit_text(
             text="▣▣▣"
         )
+        await query.message.edit_text(
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
             text=script.ABOUT_TXT.format(temp.B_NAME),
