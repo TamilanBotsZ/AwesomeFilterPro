6818e791b169ac4690f45e69ad39da5158105bfc
Sharath <121667705+TamilanBotsZ@users.noreply.github.com>
2023-02-08 21:03:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,10 +12,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
-
+API_ID = int(environ.get('API_ID', ))
+API_HASH = environ.get('')
+BOT_TOKEN = environ.get('')
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
